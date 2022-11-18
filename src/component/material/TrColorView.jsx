bf442bb3a35b51ddebb8ce028bcf7e364126eb21
lpre-ys <lpre.yak.shaving@gmail.com>
2022-11-18 16:39:49 +0900

    TrColor Form fix

--- src/component/material/TrColorView.jsx
+++ src/component/material/TrColorView.jsx
@@ -1,31 +1,39 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from "@emotion/react";
-import { useState } from "react";
+import { useCallback } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeTrColor } from "../../slice/materialSlice";
 import makeTransparentImage from "../../util/makeTransparentImage";
 
 export function TrColorView({ image, trColor, changeTrColor }) {
   const [isShowTrInput, setIsShowTrInput] = useState(false);
+  const [r, setR] = useState(trColor.r);
+  const [g, setG] = useState(trColor.g);
+  const [b, setB] = useState(trColor.b);
 
-  const handleChangeTrColor = ({ target }) => {
-    const value = parseInt(target.value);
-    if (value < 0 || value > 255) {
-      return;
-    }
-    const newColor = Object.assign({}, trColor);
-    newColor[target.dataset.color] = value;
-
+  const updateTrColor = (newColor) => {
     makeTransparentImage(image, newColor).then(({ transparent, trColor }) => {
       changeTrColor(transparent, trColor);
     });
+  };
+  const validateColor = (color) => {
+    return Object.keys(color).reduce((flag, key) => {
+      const value = parseInt(color[key]);
+      return flag && !Number.isNaN(value) && value >= 0 && value <= 255;
+    }, true);
   };
 
   return (
     <div css={styles.trColorWrapper} data-testid="tr-color-view">
       <span
         onClick={() => {
+          // stateのリセット
+          setR(trColor.r);
+          setG(trColor.g);
+          setB(trColor.b);
+          // 表示切替
           setIsShowTrInput(!isShowTrInput);
         }}
         css={styles.trColorLabel}
@@ -46,8 +54,18 @@ export function TrColorView({ image, trColor, changeTrColor }) {
             <input
               css={styles.color}
               type="text"
-              value={trColor.r}
-              onChange={handleChangeTrColor}
+              value={r}
+              onChange={({ target }) => {
+                setR(target.value);
+                const newColor = {
+                  r: parseInt(target.value),
+                  g: parseInt(g),
+                  b: parseInt(b),
+                };
+                if (validateColor(newColor)) {
+                  updateTrColor(newColor);
+                }
+              }}
               data-color="r"
               data-testid="trcv-input-r"
             />
@@ -55,8 +73,18 @@ export function TrColorView({ image, trColor, changeTrColor }) {
             <input
               css={styles.color}
               type="text"
-              value={trColor.g}
-              onChange={handleChangeTrColor}
+              value={g}
+              onChange={({ target }) => {
+                setG(target.value);
+                const newColor = {
+                  r: parseInt(r),
+                  g: parseInt(target.value),
+                  b: parseInt(b),
+                };
+                if (validateColor(newColor)) {
+                  updateTrColor(newColor);
+                }
+              }}
               data-color="g"
               data-testid="trcv-input-g"
             />
@@ -64,8 +92,18 @@ export function TrColorView({ image, trColor, changeTrColor }) {
             <input
               css={styles.color}
               type="text"
-              value={trColor.b}
-              onChange={handleChangeTrColor}
+              value={b}
+              onChange={({ target }) => {
+                setB(target.value);
+                const newColor = {
+                  r: parseInt(r),
+                  g: parseInt(g),
+                  b: parseInt(target.value),
+                };
+                if (validateColor(newColor)) {
+                  updateTrColor(newColor);
+                }
+              }}
               data-color="b"
               data-testid="trcv-input-b"
             />
@@ -103,11 +141,11 @@ const styles = {
   trColorWrapper: css`
     height: 2em;
     line-height: 1.8em;
-    cursor: pointer;
   `,
   trColorLabel: css`
     text-decoration: underline;
     text-decoration-color: #1e88e5;
+    cursor: pointer;
   `,
   color: css`
     width: 2em;
